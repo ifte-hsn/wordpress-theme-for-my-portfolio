0e39e970ddf793bda41a72cdc23eb74473cfe5eb
iftekhar_hossain <ifte.hsn@gmail.com>
2023-06-08 16:34:13 +0600

    added social links of fb, medium, yt and ln

--- footer.php
+++ footer.php
@@ -13,18 +13,17 @@
 				<div class="row d-flex align-items-center gy-4">
 					<!-- copyright text -->
 					<div class="col-md-4">
-						<span class="copyright">© 2021 Katen. Template by ThemeGer.</span>
+						<span class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo("name") ?>.</span>
 					</div>
 
 					<!-- social icons -->
 					<div class="col-md-4 text-center">
 						<ul class="social-icons list-unstyled list-inline mb-0">
-							<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+							<li class="list-inline-item"><a href="https://www.facebook.com/if.shishir/"><i class="fab fa-facebook-f"></i></a></li>
 							<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-							<li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-							<li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-							<li class="list-inline-item"><a href="#"><i class="fab fa-medium"></i></a></li>
-							<li class="list-inline-item"><a href="#"><i class="fab fa-youtube"></i></a></li>
+							<li class="list-inline-item"><a href="https://medium.com/@ifte.hsn"><i class="fab fa-medium"></i></a></li>
+							<li class="list-inline-item"><a href="https://www.youtube.com/@iftekharhossain-shishir"><i class="fab fa-youtube"></i></a></li>
+							<li class="list-inline-item"><a href="https://www.linkedin.com/in/iftekhar-hossain-369bbb6a/"><i class="fab fa-linkedin"></i></a></li>
 						</ul>
 					</div>
 
--- header.php
+++ header.php
@@ -75,12 +75,11 @@
 						<div class="col-md-8 d-none d-md-block">
 							<!-- social icons -->
 							<ul class="social-icons list-unstyled list-inline mb-0 float-end">
-								<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li class="list-inline-item"><a href="https://www.facebook.com/if.shishir/"><i class="fab fa-facebook-f"></i></a></li>
 								<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-								<li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-								<li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-								<li class="list-inline-item"><a href="#"><i class="fab fa-medium"></i></a></li>
-								<li class="list-inline-item"><a href="#"><i class="fab fa-youtube"></i></a></li>
+								<li class="list-inline-item"><a href="https://medium.com/@ifte.hsn"><i class="fab fa-medium"></i></a></li>
+								<li class="list-inline-item"><a href="https://www.youtube.com/@iftekharhossain-shishir"><i class="fab fa-youtube"></i></a></li>
+								<li class="list-inline-item"><a href="https://www.linkedin.com/in/iftekhar-hossain-369bbb6a/"><i class="fab fa-linkedin"></i></a></li>
 							</ul>
 						</div>
 
